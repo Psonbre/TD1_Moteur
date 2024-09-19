f31ad1a415a27e90a7575955514f0f56cbb64937
Psonbre <adam.maps.2003@gmail.com>
2024-09-19 17:04:03 -0400

    finished

--- player.gd
+++ player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var speed := 10000
+var idle := true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +11,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	velocity = Input.get_vector("Left", "Right", "Up", "Down") * speed * delta;
+	
+	if (Input.is_action_pressed("Up")):
+		animated_sprite_2d.play("Up")
+		idle = false
+	elif (Input.is_action_pressed("Down")):
+		animated_sprite_2d.play("Down")
+		idle = false
+	elif (Input.is_action_pressed("Left")):
+		animated_sprite_2d.play("Left")
+		idle = false
+	elif (Input.is_action_pressed("Right")):
+		animated_sprite_2d.play("Right")
+		idle = false
+	elif (idle == false):
+		idle = true
+		animated_sprite_2d.play("Idle_"+animated_sprite_2d.animation)
+	
 	move_and_slide()
